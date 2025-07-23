2713f9619193b3adde803e7b15b7a7806a9fc4c2
Bhoomik Sorathiya <patelbhumik860@gmail.com>
2025-07-23 09:52:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+    // Mobile Menu Toggle
+    function toggleMenu() {
+      document.getElementById("menu").classList.toggle("active");
+    }
+
+    // Scroll to Courses Section
+    function scrollToCourses() {
+      document.getElementById("courses").scrollIntoView({ behavior: 'smooth' });
+    }
